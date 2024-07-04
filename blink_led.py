c9c1c3c17a58a879374b6cc6766a16d4d5096738
Brendan <brendan.development@pm.me>
2024-07-04 14:26:27 -0700

    changed second led gpio

--- blink_led.py
+++ blink_led.py
@@ -3,15 +3,15 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
 try:
     while True:
         GPIO.output(17, GPIO.HIGH)
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(27, GPIO.LOW)
         sleep(.5)
         GPIO.output(17, GPIO.LOW)
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(27, GPIO.HIGH)
         sleep(.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
